7757d341d45470d00443af7d27da2b55c0dad30a
Stas <brunkovsky@gmail.com>
2016-03-09 08:30:01 +0000

    Update send_taf_to_kdp.sh

--- send_taf_to_kdp.sh
+++ send_taf_to_kdp.sh
@@ -2,7 +2,7 @@
 
 if [ -s "/home/briz/tmp/taf_mail.dat" ]
 then
-    cat /home/briz/tmp/taf_mail.dat | mutt -s "TAF Kherson" ctr_kherson@uksatse.aero
+    cat /home/briz/tmp/taf_mail.dat | mutt -s "TAF Kherson" *******@*******.***
     aplay /home/briz/etc/sounds/taf_kdp_send_mail.wav
 else
     aplay /home/briz/etc/sounds/taf_kdp_send_error.wav
